40b0a51a667f901e3d609cdf13a118c86fd1c1d5
shusheel <mjsushil.23.11@gmail.com>
2023-11-22 13:25:06 +0530

    generated and added taskview component to app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TaskviewComponent } from './pages/taskview/taskview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TaskviewComponent}
+];
---
+++ frontend/src/app/pages/taskview/taskview.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-taskview',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './taskview.component.html',
+  styleUrl: './taskview.component.scss'
+})
+export class TaskviewComponent {
+
+}
